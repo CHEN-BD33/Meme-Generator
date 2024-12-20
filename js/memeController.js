5a42ccdb86208a89137cff7066ca88fe3d5ff26a
CHEN-BD33 <chenben10.cbd@gmail.com>
2024-12-20 16:39:06 +0200

    Create meme render img and text

---
+++ js/memeController.js
@@ -0,0 +1,67 @@
+'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    renderGallery()
+
+    gElCanvas = document.querySelector('.editor-canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    window.addEventListener('resize', () => {
+        resizeCanvas()
+        coverCanvasWithImg(document.querySelector('img'))
+    })
+    
+    renderMeme()
+}
+
+function renderMeme() {
+
+    const meme = getMeme()
+    const img = new Image()
+    img.src = `images/${meme.selectedImgId}.jpg`
+
+    img.onload = () => {
+        coverCanvasWithImg(img)
+
+        meme.lines.forEach((line, idx) => {
+            drawText(line, idx)
+        })
+    }
+}
+
+function onSelectImg(elImg) {
+    const imgId = elImg.dataset.imgId
+   setImg(imgId)
+    renderMeme()
+}
+
+function coverCanvasWithImg(elImg) {
+    gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.clientWidth - 2
+}
+
+function drawText(line, idx) {
+    const { txt, size, color } = line
+    gCtx.fillStyle = color
+    gCtx.font = `${size}px Impact`
+    gCtx.textAlign = 'center'
+
+    const x = gElCanvas.width / 2
+    const y = idx === 0 ? 50 : gElCanvas.height - 50
+
+    gCtx.strokeText(txt, x, y)
+    gCtx.fillText(txt, x, y)
+}
+
+function onTextInput(txt) {
+    setLineTxt(txt)
+    renderMeme()
+}
---
+++ js/services/meme.service.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+// var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getMeme() {
+    return gMeme
+}
+
+function setImg(imgId) {
+    gMeme.selectedImgId = imgId
+}
+
+function setLineTxt(txt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = txt
+}
