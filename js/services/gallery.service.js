583c52cc40aea34e09f4af9a9c06dced1e8193dc
CHEN-BD33 <chenben10.cbd@gmail.com>
2024-12-22 10:10:27 +0200

    Add images

--- js/services/gallery.service.js
+++ js/services/gallery.service.js
@@ -2,7 +2,24 @@
 
 var gImgs = [
     { id: 1, url: 'images/1.jpg', keywords: ['funny', 'tramp'] },
-    { id: 2, url: 'images/2.jpg', keywords: ['dog'] }
+    { id: 2, url: 'images/2.jpg', keywords: ['dog'] },
+    { id: 3, url: 'images/3.jpg', keywords: ['dog'] },
+    { id: 4, url: 'images/4.jpg', keywords: ['dog'] },
+    { id: 5, url: 'images/5.jpg', keywords: ['dog'] },
+    { id: 6, url: 'images/6.jpg', keywords: ['dog'] },
+    { id: 7, url: 'images/7.jpg', keywords: ['dog'] },
+    { id: 8, url: 'images/8.jpg', keywords: ['dog'] },
+    { id: 9, url: 'images/9.jpg', keywords: ['dog'] },
+    { id: 10, url: 'images/10.jpg', keywords: ['dog'] },
+    { id: 11, url: 'images/11.jpg', keywords: ['dog'] },
+    { id: 12, url: 'images/12.jpg', keywords: ['dog'] },
+    { id: 13, url: 'images/13.jpg', keywords: ['dog'] },
+    { id: 14, url: 'images/14.jpg', keywords: ['dog'] },
+    { id: 15, url: 'images/15.jpg', keywords: ['dog'] },
+    { id: 16, url: 'images/16.jpg', keywords: ['dog'] },
+    { id: 17, url: 'images/17.jpg', keywords: ['dog'] },
+    { id: 18, url: 'images/18.jpg', keywords: ['dog'] },
+
 ]
 
 function getImgs() {
